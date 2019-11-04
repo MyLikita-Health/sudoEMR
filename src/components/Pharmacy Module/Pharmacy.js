ef58c373867f9aac77fa82794d94c1222fd2b0b4
root <root@localhost.localdomain>
2019-11-04 18:37:28 +0100

    dispensary table

--- src/components/Pharmacy Module/Pharmacy.js
+++ src/components/Pharmacy Module/Pharmacy.js
@@ -4,6 +4,7 @@ import image from '../../images/phamarcy.jpg';
 import { PharmacyGuide } from '../Guides';
 import PharmacyDashboard from './PharmacyDashboard';
 import './pharmacy.css';
+import DispensaryTable from './dispensaryTable'
 
 export default class Pharmacy extends Component {
   constructor(props) {
@@ -45,11 +46,12 @@ export default class Pharmacy extends Component {
         <div
           className="col-xs-12 col-s-12 col-md-8 col-lg-6"
           style={{ height: '85vh', border: '1px solid #007bff' }}>
-          <PharmacyDashboard
+        {/* <PharmacyDashboard 
             details={details}
             processing={processing}
             toggleProcessingForm={toggleProcessingForm}
-          />
+          /> */}
+          <DispensaryTable/>
         </div>
         <div className="col-xs-12 col-md-12 col-md-12 col-lg-3">
           <img src={image} alt="module-pic" className="module-pic" />
